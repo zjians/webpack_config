46c634f90429851644c2b2606add854fbb8cb98a
zhoujian <758507292@qq.com>
2018-10-10 14:15:54 +0800

    对html中的img引用使用html-withimg-loader做打包处理

--- webpack.dev.js
+++ webpack.dev.js
@@ -27,7 +27,7 @@ module.exports = {
                 }
             },
             {
-                test: /\.(png|jpg|gif)$/,
+                test: /\.(png|jpg|jpeg|gif)$/,
                 loader: 'url-loader',
                 options: {
                     limit: '1000',
@@ -45,6 +45,14 @@ module.exports = {
                     },
                     "css-loader"
                 ]
+            },
+            {
+                test: /\.less$/,
+                loader: 'less-loader'
+            },
+            {
+                test: /\.(htm|html)$/,
+                use: ['html-withimg-loader']
             }
         ]
     },
